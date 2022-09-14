7bffe7a6c823a4f274e10c9526037f2cf098073b
Timur Gafiulin <t.gafiulin@ya.ru>
2022-09-14 19:41:35 +0300

    add JSON.stringify

--- src/parser.js
+++ src/parser.js
@@ -1,19 +1,19 @@
 import _ from 'lodash';
 
 export default (file1, file2) => {
-  const result = [];
+  const result = {};
 
   const keys1 = _.sortBy(Object.keys(file1));
 
   for (let i = 0; i < keys1.length; i += 1) {
     const key1 = keys1[i];
     if (!file2[key1]) {
-      result.push(`- ${key1}: ${file1[key1]}`);
+      result[`- ${key1}`] = file1[key1];
     } else if (file1[key1] === file2[key1]) {
-      result.push(`${key1}: ${file1[key1]}`);
+      result[`${key1}`] = file1[key1];
     } else {
-      result.push(`- ${key1}: ${file1[key1]}`);
-      result.push(`+ ${key1}: ${file2[key1]}`);
+      result[`- ${key1}`] = file1[key1];
+      result[`+ ${key1}`] = file2[key1];
     }
   }
 
@@ -22,9 +22,9 @@ export default (file1, file2) => {
   for (let i = 0; i < keys2.length; i += 1) {
     const key2 = keys2[i];
     if (!file1[key2]) {
-      result.push(`+ ${key2}: ${file2[key2]}`);
+      result[`+ ${key2}`] = file2[key2];
     }
   }
 
-  return `{\n  ${result.join('\n  ')}\n}`;
+  return JSON.stringify(result, null, 2).replace(/[,"]/g, '');
 };
